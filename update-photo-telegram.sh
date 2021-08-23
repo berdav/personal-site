4e964b1199796ffea449b0e629178fe3df961800
Davide Berardi <berardi.dav@gmail.com>
2021-08-23 16:19:57 +0200

    Cron compatible script

--- update-photo-telegram.sh
+++ update-photo-telegram.sh
@@ -3,10 +3,10 @@
 set -eu
 
 get_photo_cdn_url() {
-	curl -L "https://t.me/$1" |
-		awk -F src= '/tgme_page_photo_image/{print $2}' |
-		sed 's/"\([^"]*\)".*/\1/'
+	/usr/bin/curl -L "https://t.me/$1" 2>/dev/null |
+		/usr/bin/awk -F src= '/tgme_page_photo_image/{print $2}' |
+		/bin/sed 's/"\([^"]*\)".*/\1/'
 }
 
 URL="$(get_photo_cdn_url "$1")"
-curl -L "$URL" > tg_profile.jpg
+/usr/bin/curl -L "$URL" > tg_profile.jpg 2>/dev/null
